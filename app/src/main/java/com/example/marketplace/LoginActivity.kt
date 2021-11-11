02a9a2cc2e7a5b37730e0d8eaf53bb6dfa90e055
camilogarzon16 <camilo.garzonl@outlook.com>
2021-11-11 18:43:50 -0500

    commit 5

--- app/src/main/java/com/example/marketplace/LoginActivity.kt
+++ app/src/main/java/com/example/marketplace/LoginActivity.kt
@@ -3,15 +3,33 @@ package com.example.marketplace
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
+    private var edt_username: EditText? = null
+    private var edt_password: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        edt_username = findViewById(R.id.edt_username)
+        edt_password = findViewById(R.id.edt_password)
     }
 
     fun onReturn(view: android.view.View) {
         val intento = Intent(this, MainActivity::class.java)
         startActivity(intento)
+    }
+
+    fun onGetIn(view: android.view.View) {
+        if (edt_username!!.text.toString()=="juli110@hotmail.es"){
+            if (edt_password!!.text.toString()=="12345"){
+
+                val intento = Intent(this, WelcomeActivity::class.java)
+                startActivity(intento)
+
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/marketplace/WelcomeActivity.kt
+++ app/src/main/java/com/example/marketplace/WelcomeActivity.kt
@@ -2,11 +2,30 @@ package com.example.marketplace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
         setSupportActionBar(findViewById(R.id.my_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_welcome_activity,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem)= when(item.itemId) {
+        R.id.action_settings -> {
+            true
+        }
+        R.id.action_search -> {
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
