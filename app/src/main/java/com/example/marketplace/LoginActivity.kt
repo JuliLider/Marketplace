1af5492032783fd96a637e77076da8a18e9ce5a8
camilogarzon16 <camilo.garzonl@outlook.com>
2021-11-18 11:26:55 -0500

    commit7

--- app/src/main/java/com/example/marketplace/LoginActivity.kt
+++ app/src/main/java/com/example/marketplace/LoginActivity.kt
@@ -1,12 +1,16 @@
 package com.example.marketplace
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.Toast
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
@@ -17,32 +21,102 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        //Analytics Event
+        val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integracion de Firebase completa")
+        analytics.logEvent("InitScreen", bundle)
+
         edt_username = findViewById(R.id.edt_username)
         edt_password = findViewById(R.id.edt_password)
         // Obtain the FirebaseAnalytics instance.
+        title = resources.getString(R.string.test_login)
+
+        session()
+
     }
 
-    fun onGetIn(view: android.view.View) {
-        var user:String=edt_username!!.text.toString()
-        var password:String=edt_password!!.text.toString()
+    override fun onStart() {
+        super.onStart()
 
-        if (user=="juli110@hotmail.es"&&password=="12345"){
+        val prefs =
+            getSharedPreferences(resources.getString(R.string.preds_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
 
-                val intento = Intent(this, WelcomeActivity::class.java)
-                startActivity(intento)
-                Toast.makeText(applicationContext,"welcome",Toast.LENGTH_LONG).show()
-
-            }
-            else{
-                Toast.makeText(this,"Invalid user name or password",Toast.LENGTH_LONG).show()
-            }
+        if (email == null && provider == null) {
+            var loginLayout = findViewById<LinearLayout>(R.id.loginLayout);
+            loginLayout.visibility = View.VISIBLE
         }
 
-    fun onReturn(view: android.view.View) {
-        val intento = Intent(this, MainActivity::class.java)
-        startActivity(intento)
+
     }
 
+    private fun session() {
+        val prefs =
+            getSharedPreferences(resources.getString(R.string.preds_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
+
+        if (email != null && provider != null) {
+            var loginLayout = findViewById<LinearLayout>(R.id.loginLayout);
+            loginLayout.visibility = View.INVISIBLE
+
+            showHome(email, ProviderType.valueOf(provider))
+        }
+
+    }
+
+    private fun showHome(username: String, provider: ProviderType) {
+
+        val homeIntent = Intent(this, WelcomeActivity::class.java).apply {
+            putExtra("email", username)
+            putExtra("provider", provider.toString())
+        }
+
+        startActivity(homeIntent)
+
+        getToast(resources.getString(R.string.test_welcome));
+    }
+
+    private fun getToast(message: String) {
+        Toast.makeText(
+            applicationContext,
+            message,
+            Toast.LENGTH_LONG
+        ).show();
+    }
+
+
+    fun onGetIn(view: android.view.View) {
+        var user: String = edt_username!!.text.toString()
+        var password: String = edt_password!!.text.toString()
+
+        if (user.isNotEmpty() && password.isNotEmpty()) {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(user, password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showHome(user, ProviderType.BASIC)
+                    } else {
+                        getToast(resources.getString(R.string.test_errorAuth));
+                    }
+                }
+
+        } else {
+            getToast(resources.getString(R.string.test_errorlogin));
+        }
+    }
+
+        fun onReturn(view: android.view.View) {
+            val intento = Intent(this, MainActivity::class.java)
+            startActivity(intento)
+        }
+
+        fun onRegistrar(view: android.view.View) {
+            val registrarIntent = Intent(this, RegisterActivity::class.java)
+            startActivity(registrarIntent)
+            getToast(resources.getString(R.string.test_register));
+        }
 
 
 }
--- app/src/main/java/com/example/marketplace/WelcomeActivity.kt
+++ app/src/main/java/com/example/marketplace/WelcomeActivity.kt
@@ -1,31 +1,102 @@
 package com.example.marketplace
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuItem
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+//import com.example.marketplace.databinding.ActivityHomeBinding
+import com.facebook.login.LoginManager
+import com.google.firebase.auth.FirebaseAuth
+
+enum class  ProviderType{
+    BASIC,
+    GOOGLE,
+    FACEBOOK
+}
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+//    private lateinit var binding: ActivityWelcomeBinding
+/*
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_welcome)
+
         setSupportActionBar(findViewById(R.id.my_toolbar))
+
+        binding.appBarHome.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_welcome_activity,menu)
-        return super.onCreateOptionsMenu(menu)
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+       // val navController = findNavController(R.id.nav_host_fragment_content_home)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        //setup Login
+        title=resources.getString(R.string.test_Home)
+        val bundle =intent.extras
+        val email=bundle?.getString("email")
+        val provider=bundle?.getString("provider")
+
+        navView.getHeaderView(0).findViewById<TextView>(R.id.emailTextView).text = email;
+        navView.getHeaderView(0).findViewById<TextView>(R.id.providerTextView).text=provider;
+
+        //Save Data
+        val prefs=getSharedPreferences(resources.getString(R.string.preds_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email",email)
+        prefs.putString("provider",provider)
+        prefs.apply()
+
     }
 
-    override fun onOptionsItemSelected(item: MenuItem)= when(item.itemId) {
-        R.id.action_settings -> {
-            true
-        }
-        R.id.action_search -> {
-            true
-        }
-        else -> {
-            super.onOptionsItemSelected(item)
-        }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.home, menu)
+        return true
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_home)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+
+    fun onSignoff(view: android.view.View) {
+
+        //Delete Data
+        val prefs=getSharedPreferences(resources.getString(R.string.preds_file), Context.MODE_PRIVATE).edit()
+        prefs.clear()
+        prefs.apply()
+
+        val navView: NavigationView = binding.navView
+        if(navView.findViewById<TextView>(R.id.providerTextView).text == ProviderType.FACEBOOK.name){
+            LoginManager.getInstance().logOut()
+        }
+
+        FirebaseAuth.getInstance().signOut()
+        onBackPressed()
+    }*/
+
 }
